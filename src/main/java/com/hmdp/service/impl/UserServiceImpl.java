d9ad0162d9282232ea500eaab7c4f3aee0147bd5
GJJ <1219076829@qq.com>
2022-10-19 17:27:48 +0800

    验证码session登录

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,23 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Random;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -15,6 +28,59 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            //不符合返回错误信息
+            return Result.fail("手机号格式错误");
+        }
+        //符合，生成
+        String code = RandomUtil.randomNumbers(6);
+        //保存验证码到session
+        session.setAttribute("code",code);
+        log.debug("验证码为：{}",code);
+        //发送验证码
+
+        //返回ok
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+
+        String phone = loginForm.getPhone();
+
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            //不符合返回错误信息
+            return Result.fail("手机号格式错误");
+        }
+        Object cachecode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if(cachecode == null || !cachecode.toString().equals(code)){
+            return Result.fail("验证码错误");
+        }
+
+        User user = query().eq("phone", phone).one();
+
+        if (user == null) {
+            user = createUserWithPhone(phone);
+        }
+
+        session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
+
+
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
 }
